6651d02c43972b143b6d00d708f89988292021e2
FoOtBaLL3R <pavlo.23.94@gmail.com>
2019-05-22 14:02:16 +0300

    create locales en and uk

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module FirstBlog
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.time_zone = 'Kyiv'
+    config.i18n.available_locales = [:uk, :en]
+    config.i18n.default_locale = :uk
+
   end
 end
