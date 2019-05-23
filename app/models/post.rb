13d93a150bdadf35bc778209d24229c43a333901
FoOtBaLL3R <pavlo.23.94@gmail.com>
2019-05-23 10:22:45 +0300

    Add carrierwave

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  mount_uploader :image, ImageUploader
   validates :title, presence: true,
                     length: { minimum: 3 }
   validates :summary, :body, presence: true,
